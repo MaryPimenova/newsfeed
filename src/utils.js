51c9d1a176b4dba00af7ce26f3edd669884e96ec
MaryPimenova <alla.pim-wolf@mail.ru>
2023-09-14 15:46:20 +0300

    added module architecture with babel and webpack technologies

---
+++ src/utils.js
@@ -0,0 +1,16 @@
+export const categoryIds = {
+    index: 0,
+    technologies: 1,
+    sport: 2,
+    fashion: 3,
+    politics: 4,
+    other: 5
+}
+
+export const categoryNames = {
+    index: 'Главная',
+    fashion: 'Мода',
+    technologies: 'Технологии',
+    politics: 'Политика',
+    sport: 'Спорт'
+}
